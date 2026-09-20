cd7c641c97a9834a79f2bf8843f9659f878505c2
Seqi <gavin2noah@gmail.com>
2026-09-20 01:37:30 -0500

    e

---
+++ Code Examples/HelloWorld.java
@@ -0,0 +1,24 @@
+public class HelloWorld {
+    @SuppressWarnings("unused")
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        String firstName = "Gavin";
+        int age = 16;
+
+        String intro = "Hello I'm Gavin, I'm ";
+
+        System.out.println(intro + age);
+
+        //single line comment
+
+        /*
+        Multi
+        Line
+        Comment 
+            */
+           
+        }
+    }
+
